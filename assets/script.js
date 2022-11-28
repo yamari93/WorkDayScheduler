e59c6a5592e90824ecb4069fe7d217c7357f7e69
yamari <yamarigonzalez25@gmail.com>
2022-11-27 23:24:24 -0600

    hour and description to show up in console log

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 $(function () {
   let buttons = $(".saveBtn");
   buttons.on("click", function (event) {
-    let parent = $(event.target).closest(".time-block");
-    console.log(parent);
+    let parent = $(this).parent().attr("id");
+    let description = $(this).siblings(".description").val();
+    console.log(parent, description);
   });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
